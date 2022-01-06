0c70441945b1c73c2b5f672cb55cde462938315e
Antony-SS <87108679+Antony-SS@users.noreply.github.com>
2022-01-06 16:57:21 +0000

    updated frontend functionality to show history of recs.  Need to add functionality to wave button so that text can be input by user.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import abi from "./utils/waveportal.json"
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [allRecs, setAllRecs] = useState([]);
 
-  const contractAddress = "0x9d302c0b2603ADC309A906928457101c27096Fd2";
+  const contractAddress = "0x001A3F375620aA1255cCC1100CFFE2eC757Bcc2e";
   const contractABI = abi.abi;
-  
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -26,6 +27,8 @@ const App = () => {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account);
+        await getAllRecs();
+
       } else {
         console.log("No authorized account found")
       }
@@ -37,6 +40,7 @@ const App = () => {
   /**
   * Implement your connectWallet method here
   */
+
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -49,13 +53,13 @@ const App = () => {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
     }
   }
 
-const wave = async () => {
+  const wave = async () => {
     try {
       const { ethereum } = window;
 
@@ -68,17 +72,18 @@ const wave = async () => {
         */
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        let count = await wavePortalContract.getTotalSomething();
-        console.log("Retrieved total something count...", count.toNumber());
+        let count = await wavePortalContract.getTotalRecs();
+        console.log("Retrieved total rec count...", count.toNumber());
 
-        const waveTxn = await wavePortalContract.doSomething();
+        const waveTxn = await wavePortalContract.makeRec("linktospotify", "Some song");
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
 
-        count = await wavePortalContract.getTotalSomething();
+        count = await wavePortalContract.getTotalRecs();
         console.log("Retrieved total something count...", count.toNumber());
+        await getAllRecs();
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -87,33 +92,83 @@ const wave = async () => {
     }
   }
 
+  const getAllRecs = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const musicPortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        // calling getAllRecs from smart contracts, this is where we read from the smart contract we have deployed.  It is almost acting as a server.
+        const recs = await musicPortalContract.getAllRecs();
+        console.log("original recs from contract", recs)
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let formattedrecs = [];
+        recs.forEach(rec => {
+          formattedrecs.push({
+            reccomender: rec.reccomender,
+            timestamp: new Date(rec.timestamp * 1000),
+            link: rec.link,
+            description: rec.description
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllRecs(formattedrecs);
+        console.log(formattedrecs);
+        console.log("updated to all recs to ...", allRecs);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
-  
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-          Hi, I'm Antony.  This is my first Dapp.  I'm looking for some music reccomendations.  Enter a Spotify or Soundcloud link below!  Don't worry this is only on the testnet (switch to Rinkeby in Metamask) so waving doesn't cost any real money.  
+          Hi, I'm Antony and this is my first Dapp. . .  I'm looking for some music reccomendations.  Drop a Spotify or Soundcloud link below!  Don't worry this is on the testnet (switch to Rinkeby Network on Metamask) so making a reccomendation doesn't cost any real money.
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Do Something
+          Drop a song reccomendation!
         </button>
-        
+
         {/*
-        * If there is no currentAccount render this button
+        * If there is no account render the connect wallet button
         */}
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
             Connect Wallet
           </button>
         )}
+
+        {allRecs.map((rec, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {rec.reccomender}</div>
+              <div>Time: {rec.timestamp.toString()}</div>
+              <div>link: {rec.link}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
